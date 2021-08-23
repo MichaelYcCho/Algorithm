26d3198e1e4971f8dbed9d76831ad3128024e3d3
Youngchan <michael_cho77@naver.com>
2021-08-23 23:48:04 +0900

    K번째 큰 수

--- 파이썬알고리즘문제풀이/Section2-3/AA.py
+++ 파이썬알고리즘문제풀이/Section2-3/AA.py
@@ -1,4 +1,23 @@
 import sys
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+sum_list = []
+
+for i in range(n):
+    for j in range(i+1, n):
+        for m in range(j+1, n):
+            result = a[i] + a[j] + a[m]
+            sum_list.append(result)
+
+#reverse를 통해 내림차순으로 정렬
+sum_list.sort(reverse=True)
+#print(sum_list)
+print(sum_list[k-1])
+
+
+'''
+모범답안
 #sys.stdin = open("input.txt", "rt")
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
@@ -13,3 +32,4 @@ for i in range(n):
 res = list(res)
 res.sort(reverse=True)
 print(res[k-1])
+'''

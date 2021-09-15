1755cf4dade34661c8d92f06ccf2a40678c34227
Youngchan <michael_cho77@naver.com>
2021-09-15 23:52:48 +0900

    사과나무

---
+++ 파이썬알고리즘문제풀이/Section3-7/AA.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.stdin = open('input.txt', 'r')
+
+n = int(input())
+
+a = [list(map(int, input().split())) for _ in range(n)]
+
+res = 0
+# s = start, e = end
+s = e = n // 2
+for i in range(n):
+    for j in range(s, e+1):
+        res += a[i][j]
+    if i < n // 2:
+        s -= 1
+        e += 1
+    else:
+        s += 1
+        e -= 1
+
+print(res)

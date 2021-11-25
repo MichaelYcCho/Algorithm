c0b7afda26169f3ed950cee8ee11064a7f6fb3cf
Youngchan <michael_cho77@naver.com>
2021-11-25 23:41:44 +0900

    3-9 복습

--- 파이썬알고리즘문제풀이/Section3-9/AA.py
+++ 파이썬알고리즘문제풀이/Section3-9/AA.py
@@ -1,4 +1,41 @@
 import sys
+
+sys.stdin = open('input.txt', 'r')
+
+n = int(input())
+
+matrix = [list(map(int, input().split())) for _  in range(n)]
+
+
+m = [[0] * (n+2) for _ in range(n+2)]
+
+
+
+for i in range(n):
+    for j in range(n):
+        m[i+1][j+1] = matrix[i][j]
+        
+print(m)        
+        
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+
+cnt =0
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if all(m[i][j] > m[i + dx[k]] [j + dy[k]]  for k in range(4)):
+            cnt +=1
+
+
+print(cnt)
+
+
+
+
+
+'''
+import sys
 sys.stdin=open("input.txt","rt")
  
 n=int(input())
@@ -26,7 +63,7 @@ for x in range(n+2):
                 res+=1
  
 print(res)
-
+'''
 
 
 

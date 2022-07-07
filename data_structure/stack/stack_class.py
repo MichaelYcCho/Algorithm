a1925061a7a1eea71a344793ea3df2f47aac6032
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-07-07 21:57:59 +0900

    add: 선형 Queue

--- data_structure/stack/stack_class.py
+++ data_structure/stack/stack_class.py
@@ -12,15 +12,17 @@ class Stack:
             return False
 
     def push(self, data):
+         # 마지막 위치에 값을 삽입하므로 O(1) 
         self.container.append(data)
 
     def pop(self):
         if self.empty():
             return None
         else:
+            # 마지막 값을 빼는것이므로 O(1) 
             return self.container.pop()
 
-    # 스택 가장 위의 데이터를 반환
+    # 스택 가장 위의 데이터를 반환 특정데이터를 찾을때까지 반복하므로 O(n)
     def peek(self):
         if self.empty():
             return None

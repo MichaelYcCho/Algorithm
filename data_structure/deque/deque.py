13a581912aec711a78f8f11d95b084f600c536f8
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-07-08 21:23:19 +0900

    add: Deque

---
+++ data_structure/deque/deque.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+from collections import deque
+
+print('*' * 20 + ' STACK ' + '*' * 20)
+stack = deque()
+for i in range(1, 6):
+    stack.append(i)
+    print(stack)
+
+for i in range(5):
+    print(stack.pop())
+
+print('*' * 20 + ' QUEUE ' + '*' * 20)
+queue = deque()
+for i in range(1, 6):
+    queue.append(i)
+    print(queue)
+
+for i in range(5):
+    # front에서 데이터를 출력 
+    print(queue.popleft())

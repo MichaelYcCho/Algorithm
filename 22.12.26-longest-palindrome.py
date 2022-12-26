ea3fff1403ab75bdf0c7cf5a4583fcb5c7ad172b
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-12-26 23:47:59 +0900

    docs: 주석

--- 22.12.26-longest-palindrome.py
+++ 22.12.26-longest-palindrome.py
@@ -1,8 +1,22 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str :
+    def longestPalindrome1(self, s: str) -> str :
 
         def isPalin(s1):
             """ 팰린드롬인지 확인하는 함수 """
             if s1[::-1] == s1:
                 return True
             return False
+
+        """ 첫글자를 기준으로 한칸씩 포인터가 뒤로가면서 모두 순회하면서 팰린드롬이면 answer에 저장하는 형태 """
+        answer = ""
+        n = len(s)
+        for i in range(n):
+            for j in range(i, n):
+                subStr = s[i : j+1]
+                if isPalin(subStr) and len(subStr) > len(answer):
+                    answer = s[i : j+1]
+        return answer
+
+
+solution = Solution()
+print(solution.longestPalindrome1("babad")) # bab

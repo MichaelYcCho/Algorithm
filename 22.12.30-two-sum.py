5b3392648f375c4fc0d00ba35fc0db4f0f044fe5
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-12-31 23:52:37 +0900

    feat: 두수의 합 Case3

--- 22.12.30-two-sum.py
+++ 22.12.30-two-sum.py
@@ -24,13 +24,25 @@ class Solution:
             if least_num in nums[num + 1:]:
                 # 잔여 숫자에 index가 num+1 부터 시작해 0으로 초기화 되므로 나온 인덱스값에서 (num+1)만큼 더해준다
                 return [nums.index(value), nums[num+ 1:].index(least_num) + (num + 1)]
- 
+
+    def twoSum3(self, nums: List[int], target: int) -> List[int]:
+        # twoSum2에서 시간복잡도를 개선해보자
+        # in 의 시간복잡도 O(n)
+
+        hash_map = {}
+        # 키와 값을 바꿔서 딕셔너리로 저장
+        for num, value in enumerate(nums): 
+            hash_map[value] = num
+        
+        # 타겟에서 첫번째 수를 뺀 결과를 키로 조회
+        for num, value in enumerate(nums):
+            if (target - value) in hash_map and num != hash_map[target - value]:
+                return [num, hash_map[target - value]]
 
 
 
-
-
+  
 solution=Solution()
-answer = solution.twoSum2([11,2,15,7], 9)
+answer = solution.twoSum3([11,2,15,7], 9)
 #output -> [1, 3]
 print(answer)

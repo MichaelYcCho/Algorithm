303aab46db9bce731f14c71ae67a5f0e3a55f8b7
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-03-12 22:58:22 +0900

    feat: LinkedList get()

--- 23.01.09[tip]-linked-list.py
+++ 23.01.09[tip]-linked-list.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
+from typing import Optional
+
+
 class Node:
-    def __init__(self, value=0, next=None) -> None:
+    def __init__(self, value: int = 0, next: Optional[Node] = None) -> None:
         self.value = value
         self.next = next
 
@@ -17,7 +21,8 @@ class LinkedList:
     def __init__(self) -> None:
         self.head = None
 
-    def append(self, value):
+    # Head를 통해서 맨 뒤에 node를 추가해야하므로 O(n)이다.
+    def append(self, value: int) -> None:
         new_node = Node(value)
         if self.head is None:
             # head는 항상 첫번째 노드만 가리켜야한다.
@@ -29,6 +34,13 @@ class LinkedList:
             while current.next:
                 current = current.next
             current.next = new_node
+
+    # Head를 통해서 해당하는 인덱스 까지가야하므로 O(n)이다.
+    def get(self, idx: int) -> int:
+        current = self.head
+        for i in range(idx):
+            current = current.next
+        return current.value
 
 
 ll = LinkedList()

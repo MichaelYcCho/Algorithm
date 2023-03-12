1b0321863f58100cba8253dad8eb1440c0f3cd15
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-03-12 22:37:38 +0900

    style: Black

--- 23.01.09[tip]-linked-list.py
+++ 23.01.09[tip]-linked-list.py
@@ -1,8 +1,8 @@
 class Node:
-    def __init__(self, value = 0, next = None) -> None:
+    def __init__(self, value=0, next=None) -> None:
         self.value = value
         self.next = next
-    
+
 
 first = Node(1)
 second = Node(2)
@@ -13,20 +13,20 @@ second.next = third
 first.value = 6
 
 
-
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
+
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
             # head는 항상 첫번째 노드만 가리켜야한다.
             self.head = new_node
-        # 맨 뒤에 node가 new_node를 가리켜야한다. 
+        # 맨 뒤에 node가 new_node를 가리켜야한다.
         else:
             current = self.head
             # current.next가 None이 될때까지 반복하며 current를 이동시킨다.
-            while(current.next):
+            while current.next:
                 current = current.next
             current.next = new_node
 

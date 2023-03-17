462b03c32c0426d24bf1f7c91d4b4dc5d80cfacf
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-03-17 22:11:11 +0900

    feat: LinkedList를 활용한 Stack 구현

--- 23.03.16-stack.py
+++ 23.03.16-stack.py
@@ -34,3 +34,51 @@ s.push(3)
 print(s.pop())
 print(s.pop())
 print(s.pop())
+
+
+class Node:
+    def __init__(self, data: int) -> None:
+        self.data = data
+        self.next = None
+
+
+class LinkedStack:
+    def __init__(self) -> None:
+        self.head = None
+
+    def push(self, data: int) -> None:
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def pop(self) -> int:
+        if self.is_empty():
+            raise Exception("Stack is Empty")
+        pop_data = self.head.data
+        self.head = self.head.next
+        return pop_data
+
+    def top(self) -> int:
+        if self.is_empty():
+            raise Exception("Stack is Empty")
+        top_data = self.head.data
+        return top_data
+
+    def is_empty(self) -> bool:
+        is_empty = False
+        if self.head is None:
+            is_empty = True
+        return is_empty
+
+
+# example
+print()
+print("LinkedStack")
+s = LinkedStack()
+s.push(1)
+s.push(2)
+s.push(3)
+print(s.pop())
+print(s.pop())
+print(s.pop())
+# print(s.pop()) # Exception

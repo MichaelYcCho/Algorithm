007a49d6b1c35a003907a8395ba3c7469935cc8a
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-03-21 22:52:24 +0900

    feat: 프로그래머스 김서방찾기 (1)

---
+++ 23.03.21-programmers_12919.py
@@ -0,0 +1,18 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12919
+
+
+test_cases = ["Jane", "Kim", "Merry", "Mark", "Ki"]
+
+
+def solution_01(seoul):
+    result = None
+    for index, value in enumerate(seoul):
+        if value == "Kim":
+            result = index
+            break
+
+    answer = f"김서방은 {result}에 있다"
+    return answer
+
+
+print(solution_01(test_cases))

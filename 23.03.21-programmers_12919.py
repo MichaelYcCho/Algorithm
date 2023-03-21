d277726c9791dabffa5dd8c84a9e8cebe27bda01
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-03-21 22:55:11 +0900

    feat: 프로그래머스 김서방찾기(2)

--- 23.03.21-programmers_12919.py
+++ 23.03.21-programmers_12919.py
@@ -15,4 +15,10 @@ def solution_01(seoul):
     return answer
 
 
+def solution_02(seoul):
+    answer = f"김서방은 {seoul.index('Kim')}에 있다"
+    return answer
+
+
 print(solution_01(test_cases))
+print(solution_02(test_cases))

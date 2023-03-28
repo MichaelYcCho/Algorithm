722fb1259721e807ad75b8fb54c7c8dda8f70082
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-03-28 22:15:43 +0900

    feat: 프로그래머스 12917 정렬

--- programmers_lv1/23.03.28_12912_12944_12917.py
+++ programmers_lv1/23.03.28_12912_12944_12917.py
@@ -29,3 +29,8 @@ def solution(arr):
     sum_num = sum(arr)
 
     return sum_num / count
+
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/12917
+def solution(s):
+    return "".join(sorted(s, reverse=True))

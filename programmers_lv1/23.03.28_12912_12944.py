6075f9ada38bbf84bd5ede39c3a03c7086445ff3
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-03-28 22:10:45 +0900

    feat: 프로그래머스 12944

--- programmers_lv1/23.03.28_12912_12944.py
+++ programmers_lv1/23.03.28_12912_12944.py
@@ -21,3 +21,11 @@ def solution(a: int, b: int):
     if a > b:
         a, b = b, a
     return sum(range(a, b + 1))
+
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/12944
+def solution(arr):
+    count = len(arr)
+    sum_num = sum(arr)
+
+    return sum_num / count

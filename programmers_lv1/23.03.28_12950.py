e8eb371577d5f6468b9f7dbc5afd02f02383cf99
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-03-29 22:01:21 +0900

    feat: 프로그래머스 12950 행렬덧셈

---
+++ programmers_lv1/23.03.28_12950.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12950
+
+
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        temp = []
+        for j in range(len(arr1[0])):
+            temp.append(arr1[i][j] + arr2[i][j])
+        answer.append(temp)
+    return answer

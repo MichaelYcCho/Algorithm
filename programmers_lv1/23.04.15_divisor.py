12eee02f5a039be32bbf7c05a17b865c89916946
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-04-15 13:12:11 +0900

    feat: 약수 방법2

--- programmers_lv1/23.04.15_divisor.py
+++ programmers_lv1/23.04.15_divisor.py
@@ -5,3 +5,8 @@ def solution(n):
         if n % i == 0:
             answer += i
     return answer
+
+
+def sumDivisor(num):
+    # num / 2 의 수들만 검사하면 성능 약 2배 향상잼
+    return num + sum([i for i in range(1, (num // 2) + 1) if num % i == 0])

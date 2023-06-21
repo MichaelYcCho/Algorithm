e410faab89723539710b7b77218983426148f0a9
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-06-22 00:02:12 +0900

    feat: 제일 작은수 제거하기 (2)

--- programmers_lv1/23.06.19_86951.py
+++ programmers_lv1/23.06.19_86951.py
@@ -14,3 +14,14 @@ def solution(numbers):
         if i not in numbers:
             answer += i
     return answer
+
+
+
+def rm_small(mylist):
+    # 함수를 완성하세요
+    return [i for i in mylist if i > min(mylist)]
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+my_list = [4, 3, 2, 1]
+print("결과 {} ".format(rm_small(my_list)))

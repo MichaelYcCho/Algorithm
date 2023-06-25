0cc362120aad813db4057cac8835ddd36719b017
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-06-25 11:32:11 +0900

    feat: 문자열다루기 - 다른방법

--- programmers_lv1/23.06.24_12918.py
+++ programmers_lv1/23.06.24_12918.py
@@ -6,3 +6,11 @@ def solution(s):
             return False
     else:
         return False
+    
+
+def alpha_string46(s):
+    return s.isdigit() and len(s) in [4,6]
+
+
+print( alpha_string46("a234") )
+print( alpha_string46("1234") )

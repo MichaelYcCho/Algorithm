134e45c170826f2e270dec84bcd90e0dd668c864
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-06-26 01:42:11 +0900

    feat: 등차수열 특정항 - zip사용

--- programmers_lv1/23.06.26_181931.py
+++ programmers_lv1/23.06.26_181931.py
@@ -6,3 +6,6 @@ def solution(a, d, included):
     return total_sum
 
 
+def solution(a, d, included):
+    sequence = [a + d * i for i in range(len(included))]
+    return sum(val for val, inc in zip(sequence, included) if inc)

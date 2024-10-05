bbc2f9add98e960a0149988e2cf98e8213e56b16
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-10-06 00:00:11 +0900

    feat: 배열의평균값

---
+++ programmers/배열의평균값.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    return sum(numbers) / len(numbers)
+
+
+def solution(numbers):
+    sum1 = 0
+    for i in numbers:
+        sum1 += i
+    answer = sum1 / len(numbers)
+
+    return answer

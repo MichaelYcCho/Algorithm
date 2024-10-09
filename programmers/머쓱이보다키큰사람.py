0a39f82cb6648ca940d921f9a326414eb14342ea
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-10-10 00:00:11 +0900

    feat 머쓱이(2)

--- programmers/머쓱이보다키큰사람.py
+++ programmers/머쓱이보다키큰사람.py
@@ -4,3 +4,10 @@ def solution(array, height):
         if i > height:
             answer += 1 
     return answer
+
+
+
+def solution(array, height):
+    array.append(height)
+    array.sort(reverse=True)
+    return array.index(height)

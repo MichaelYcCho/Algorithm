86387c66a5f8eb4d3a4903458bab90046b69af65
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-10-11 00:00:11 +0900

    feat: 최댓값만들기(1)

---
+++ programmers/최댓값만들기(1).py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    numbers.sort(reverse=True)
+    a = numbers[0]
+    b = numbers[1]
+    return a * b
+
+
+def solution(numbers):
+    numbers.sort()
+    return numbers[-2] * numbers[-1]

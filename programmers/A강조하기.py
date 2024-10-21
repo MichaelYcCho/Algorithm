637c48e3d0b04a64328b3a35c576873d7da2a039
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-10-21 22:39:35 +0900

    feat: A강조하기

---
+++ programmers/A강조하기.py
@@ -0,0 +1,9 @@
+def solution(myString):
+    return myString.lower().replace("a", "A")
+
+
+def solution(myString):
+    answer = ""
+    answer = myString.lower()
+    answer = answer.replace("a", "A")
+    return answer

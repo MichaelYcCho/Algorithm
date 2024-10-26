8184960e5f374f861e5e5ea8e429fa27dde999dc
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-10-27 00:00:11 +0900

    feat: 간단한식 계산하기

---
+++ programmers/간단한식계산하기.py
@@ -0,0 +1,13 @@
+def solution(binomial):
+    a, op, b = binomial.split()
+    if op == "+":
+        return int(a) + int(b)
+    if op == "-":
+        return int(a) - int(b)
+    if op == "*":
+        return int(a) * int(b)
+    if op == "/":
+        return int(a) / int(b)
+
+
+solution = eval

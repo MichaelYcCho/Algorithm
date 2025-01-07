7f36baf3f26ff572c108137647506681cb144cb8
youngchan-itall <michael.yc.cho@gmail.com>
2025-01-08 02:00:11 +0900

    feat: 공백으로 구분하기2

---
+++ programmers/lv0/공백으로 구분하기2.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    return my_string.split()
+
+
+def solution(my_string):
+    my_string = my_string.strip()
+    words = my_string.split()
+    return words
+
+
+solution = lambda x: x.split()
